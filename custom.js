eac9e3af7e52c09397c1ca66d556932894542cfd
dishowrov <dinislamshowrov.2001@gmail.com>
2024-01-10 15:19:10 +0600

    done the tab sec

---
+++ custom.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tabContents = document.querySelectorAll(
+        ".dis-tab-container .dis-bs-card"
+    );
+    function filterTabsByCategory(category) {
+        tabContents.forEach((tabContent) => {
+            if (
+                tabContent.getAttribute("data-category") === category ||
+                category === "All"
+            ) {
+                tabContent.style.display = "flex";
+            } else {
+                tabContent.style.display = "none";
+            }
+        });
+    }
+    document
+        .getElementById("dis-cat")
+        .addEventListener("change", function () {
+            const selectedCategory = this.value;
+            filterTabsByCategory(selectedCategory);
+        });
+    const initialCategory = document.getElementById("dis-cat").value;
+    filterTabsByCategory(initialCategory);
+});
