0c3e429d55c141d5380e335cd6bbb5c31815b108
Din Islam Showrov <dinislamshowrov.2001@gmail.com>
2024-03-13 12:03:28 +0600

    added script for option tags

--- custom.js
+++ custom.js
@@ -1,25 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const tabContents = document.querySelectorAll(
-        ".dis-tab-container .dis-bs-card"
-    );
-    function filterTabsByCategory(category) {
-        tabContents.forEach((tabContent) => {
-            if (
-                tabContent.getAttribute("data-category") === category ||
-                category === "All"
-            ) {
-                tabContent.style.display = "flex";
-            } else {
-                tabContent.style.display = "none";
-            }
-        });
+// document.addEventListener("DOMContentLoaded", function () {
+//     const tabContents = document.querySelectorAll(
+//         ".dis-tab-container .dis-bs-card"
+//     );
+//     function filterTabsByCategory(category) {
+//         tabContents.forEach((tabContent) => {
+//             if (
+//                 tabContent.getAttribute("data-category") === category ||
+//                 category === "All"
+//             ) {
+//                 tabContent.style.display = "flex";
+//             } else {
+//                 tabContent.style.display = "none";
+//             }
+//         });
+//     }
+//     document
+//         .getElementById("dis-cat")
+//         .addEventListener("change", function () {
+//             const selectedCategory = this.value;
+//             filterTabsByCategory(selectedCategory);
+//         });
+//     const initialCategory = document.getElementById("dis-cat").value;
+//     filterTabsByCategory(initialCategory);
+// });
+
+function redirectToPage(selectElement) {
+    var selectedValue = selectElement.value;
+    if (selectedValue) {
+      window.location.href = selectedValue;
     }
-    document
-        .getElementById("dis-cat")
-        .addEventListener("change", function () {
-            const selectedCategory = this.value;
-            filterTabsByCategory(selectedCategory);
-        });
-    const initialCategory = document.getElementById("dis-cat").value;
-    filterTabsByCategory(initialCategory);
-});
+  }
